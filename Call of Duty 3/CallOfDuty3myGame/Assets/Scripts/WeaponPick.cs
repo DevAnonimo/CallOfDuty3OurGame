e4e7aa1d8f33603893ecbfc780592176d339e8aa
Guilherme Simionato <47949737+DevAnonimo@users.noreply.github.com>
2020-12-19 00:54:36 -0300

    Gameplay
    Gameplay
    -Mirar -- OK!
    -Pegar arma - Sendo trabalhado

---
+++ Call of Duty 3/CallOfDuty3myGame/Assets/Scripts/WeaponPick.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPick : MonoBehaviour
+{
+    public Transform equipPosition;
+    public float distance = 10f;
+    GameObject currentWeapon;
+    GameObject wp;
+
+    bool canGrab;
+
+    public GameObject m1Garant;
+
+    private void Update()
+    {
+        CheckWeapons();
+
+        if (canGrab)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (currentWeapon != null)
+                    Drop();
+
+                Desactivate();
+
+                m1Garant.SetActive(true);
+            }
+        }
+
+        if (currentWeapon != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                Drop();
+        }
+    }
+
+    private void CheckWeapons()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, distance))
+        {
+            if (hit.transform.tag == "CanGrab")
+            {
+                Debug.Log("I can grab it!");
+                canGrab = true;
+                wp = hit.transform.gameObject;
+
+            }
+        }
+        else
+            canGrab = false;
+    }
+
+    private void Desactivate()
+    {
+        currentWeapon = wp;
+        currentWeapon.SetActive(false);
+    }
+
+    private void Drop()
+    {
+    }
+}
---
+++ Call of Duty 3/CallOfDuty3myGame/Assets/Scripts/scope.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scope : MonoBehaviour
+{
+    public Animator animator;
+
+    private bool isScoped = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            isScoped = !isScoped;
+            animator.SetBool("Scope", isScoped);
+        }
+    }
+}
